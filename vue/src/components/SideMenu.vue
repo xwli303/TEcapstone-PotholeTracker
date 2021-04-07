5b44add6f00388f1a4e78a80a976874cd338c593
Steven Jenkins-Edwards <steven.brett.edwards@gmail.com>
2021-04-07 17:07:37 -0400

    sidebar stuff

---
+++ vue/src/components/SideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="side-container">
+        <div class="topline">HOME</div>
+        <div class="add-pothole">
+            <form id="report-pothole">
+                <input type="text">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SideMenu"
+}
+</script>
+
+<style>
+.side-container {
+    display: grid;
+    grid-template-areas: 
+        "topline"
+        "add-pothole"
+        "add-pothole"
+        "add-pothole";
+}
+.add-pothole {
+    grid-area: add-pothole;
+}
+.topline {
+    grid-area: topline;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-    <pothole-map />
+    <div class="home-container">
+      <side-menu class="side-menu"/>
+      <pothole-map class="pothole-map" />
+    </div>
   </div>
 </template>
 
 <script>
 import PotholeMap from "@/components/PotholeMap";
+import SideMenu from "@/components/SideMenu";
 import PotholeService from "@/services/PotholeService";
 export default {
   name: "home",
-  components: { PotholeMap },
+  components: 
+  { 
+    PotholeMap,
+    SideMenu
+  },
   created(){
         PotholeService.listPotholes()
         .then((response) => {
@@ -22,5 +28,29 @@ export default {
 </script>
 
 <style>
- 
+.home {
+  height: 100%;
+  width: 100%;
+}
+.home-container {
+  display: grid;
+  grid-template-areas: 
+    "side-menu pothole-map pothole-map pothole-map pothole-map";
+  width: 100%;
+  margin: 0 auto;
+}
+.side-menu {
+  grid-area: side-menu;
+  width: 20%;
+  position: absolute;
+  right: 0;
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+}
+.pothole-map {
+  grid-area: pothole-map;
+}
 </style>
