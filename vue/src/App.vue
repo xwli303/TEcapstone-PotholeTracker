a9dff2f1854961cc311be03f5d5a033e082d75f7
Dennis Angelisanti <dennisangelisanti@gmail.com>
2021-04-09 13:42:16 -0400

    little style

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      <router-link v-bind:to="{ name: 'pothole-view'}"> List </router-link>
+    <div id="flexbox" v-if="$store.state.token != ''">
+      <div id="icon">
+        <h2>our image</h2>
+        </div>
+      <div id="nav">
+      <router-link class="top"  v-bind:to="{ name: 'home' }">Home | </router-link>
+      <router-link class="top" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"> Logout |</router-link>
+      <router-link class="top" v-bind:to="{ name: 'pothole-view'}"> List </router-link>
+      </div>
+      
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-  #nav{
-    position: absolute;
+  #flexbox{
+    display:flex;
+    justify-content: space-between;
+    padding-right: 30px;
+    margin: 0 auto;
+    padding-inline-start: 10px;
     z-index: 1;
-    height: 15%;
-    background-color: aqua;
+    height: 100px;
+    background-color: white;
+
   }
-  
+/* unvisited link */
+#nav {
+
+}
+
+.top {
+font-family: Helvetica, sans-serif;
+font-size: 25px;
+font-weight: bold;
+color:black;
+text-decoration: none;
+vertical-align: middle;
+margin-top: 50px;
+}
+
+/* mouse over link */
+.top:hover {
+color: grey;
+text-decoration: none;
+}
+
 </style>
