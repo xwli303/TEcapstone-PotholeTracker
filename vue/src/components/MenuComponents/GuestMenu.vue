adf88d8549246b8abafa6b8c988f4a770ba7248e
Jess Larson <jessicadlarson@icloud.com>
2021-04-13 10:55:47 -0400

    Guest menu update

--- vue/src/components/MenuComponents/GuestMenu.vue
+++ vue/src/components/MenuComponents/GuestMenu.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="container">
-    <p id="welcome">Welcome! <br> <br> Thank you for your interest in improving our city. <br><br> • To see currently reported potholes, please see markers on the map.<br> <br>• To report a pothole, please create an account and log in. <br><br> Our team will address fixing potholes as soon as we can. </p>
+    <p id="welcome">Welcome! <br><br> Thank you for your interest in improving our city.<br> <br>• To see currently reported potholes, please see the map.<br> <br>• Pothole severity is as follows: </p>
+      <div id="green-marker">
+        <img class = "marker" src="@/assets/1.png" alt = "logo">
+        <p> Least Severe (1)</p>
+        <br>
+      </div>
+       <div id="yellow-marker">
+        <img class = "marker" src="@/assets/2.png" alt = "logo">
+        <p> Medium Severity (2)</p>
+        <br>
+      </div>
+       <div id="orange-marker">
+        <img class = "marker" src="@/assets/3.png" alt = "logo">
+        <p> High Severity (3)</p>
+        <br>
+      </div>
+     <div id="red-marker">
+        <img id ="red-marker-image" class = "marker" src="@/assets/4.png" alt = "logo"> 
+        <p>  Maximum Severity (4 - 5)</p>
+        <br>
+      </div>
+      <p>• To report a pothole, please create an account and log in. <br><br> Our team will address the potholes as soon as we can.</p>
   </div>
 </template>
 
@@ -16,5 +37,14 @@ export default {
 }
 p{
     font-size: 20px;
+}
+#red-marker, #green-marker, #yellow-marker, #orange-marker{
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+#red-marker-image{
+    padding-right: 4px;
+    padding-left: 5px;
 }
 </style>
